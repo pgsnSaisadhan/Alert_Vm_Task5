dc48473ea655fc4c1890ff6260a78b907995297a
pgsnSaisadhan <skodurupaka@pgsnsofttech.com>
2026-05-11 15:59:59 +0530

    only terraform new rg name

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 # RESOURCE GROUP
 # =========================
 resource "azurerm_resource_group" "app_rg" {
-  name     = "monitor-demo-rg1"
+  name     = "monitor-demo-rg2"
   location = "East US"
 }
 
